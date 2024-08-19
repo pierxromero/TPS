67c4b0b6899c6171b489df28a84bca95a2361f02
Clase-Programacion <pierofelixvisitacionromero@gmail.com>
2024-08-19 03:29:40 -0300

    TP-1 Finish TP-2 Finish

---
+++ TP-2/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     double balanceNow;
+     Scanner scanner = new Scanner(System.in);
+     BancaryAccount user1 = new BancaryAccount();
+       user1.setBalance(15000);
+       user1.setIdentifier("898A");
+     System.out.print("Enter the userName:");
+     user1.setName(scanner.nextLine());
+       System.out.println(user1.toString());
+     System.out.print("Enter the money credit:");
+     balanceNow= user1.credit(scanner.nextDouble());
+        System.out.println(user1.toString());
+        System.out.printf("\n(Main) Balance:%.2f",balanceNow);
+     System.out.print("\nEnter the money to pay:");
+       balanceNow=user1.debit(scanner.nextDouble());
+        System.out.println(user1.toString());
+        System.out.printf("\n(Main) Balance:%.2f",balanceNow);
+     System.out.println("\nMaking a purchase of 30.000");
+        balanceNow=user1.debit(30000);
+        System.out.println(user1.toString());
+        System.out.printf("\n(Main) Balance:%.2f",balanceNow);
+    }
+}
